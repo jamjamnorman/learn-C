ac6b4a5206a3f6aafc27d87df87584af8bedda99
Jamie Harvey <jamjamnorman@gmail.com>
2014-04-11 00:12:38 +1200

    Added unary negation

--- Chapter 7/evaluation.c
+++ Chapter 7/evaluation.c
@@ -55,6 +55,9 @@ long eval(mpc_ast_t* t) {
     long x = eval(t->children[2]);
 
     /* Iterate the remaining children, combining using our operator */
+    if (strcmp(op, "-") == 0 && !(strstr(t->children[3]->tag, "expr"))) {
+        return -x;
+    }
     int i = 3;
     while (strstr(t->children[i]->tag, "expr")) {
         x = eval_op(x, op, eval(t->children[i]));
