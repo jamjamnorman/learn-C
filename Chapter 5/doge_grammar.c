0071ba245dd6553300da6a1e6cc49527ece01625
Jamie Harvey <jamjamnorman@gmail.com>
2014-04-10 16:01:15 +1200

    Added .gitignore and mpc; fixed multiline strings

--- Chapter 5/doge_grammar.c
+++ Chapter 5/doge_grammar.c
@@ -8,11 +8,11 @@ int main(int argc, char** argv) {
     mpc_parser_t* Doge      = mpc_new("doge");
 
     mpca_lang(MPC_LANG_DEFAULT,
-        "
-            adjective : \"wow\" | \"many\" | \"so\" | \"such\";
-            noun      : \"lisp\" | \"language\" | \"c\" | \"book\" | \"build\";
-            phrase    : <adjective> <noun>;
-            doge      : <phrase>*;
+        "                                                                       \
+            adjective : \"wow\" | \"many\" | \"so\" | \"such\";                 \
+            noun      : \"lisp\" | \"language\" | \"c\" | \"book\" | \"build\"; \
+            phrase    : <adjective> <noun>;                                     \
+            doge      : <phrase>*;                                              \
         ",
         Adjective, Noun, Phrase, Doge);
 
