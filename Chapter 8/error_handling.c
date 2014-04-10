eebda98ac7fd43fced3c6ad8112823b5803cf7f8
Jamie Harvey <jamjamnorman@gmail.com>
2014-04-11 01:43:06 +1200

    Added union; Named enums

--- Chapter 8/error_handling.c
+++ Chapter 8/error_handling.c
@@ -25,31 +25,43 @@ void add_history(char* unused) {}
 
 #endif
 
+/* Declare union to store data in lval */
+typedef union {
+    long num;
+    int err;
+} lval_data;
+
 /* Declare new lval Struct */
 typedef struct {
     int type;
-    long num;
-    int err;
+    lval_data data;
 } lval;
 
 /* Create Enumeration of Possible lval Types */
-enum { LVAL_NUM, LVAL_ERR };
+enum lval_types { 
+        LVAL_NUM, 
+        LVAL_ERR 
+};
 
 /* Create Enumeration of Possible Error Types */
-enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+enum lval_err_types { 
+    LERR_DIV_ZERO, 
+    LERR_BAD_OP, 
+    LERR_BAD_NUM
+};
 
 /* Create a new number type lval */
 lval lval_num(long x) {
     lval v;
     v.type = LVAL_NUM;
-    v.num = x;
+    v.data.num = x;
     return v;
 }
 
 lval lval_err(int x) {
     lval v;
     v.type = LVAL_ERR;
-    v.err = x;
+    v.data.err = x;
     return v;
 }
 
@@ -57,15 +69,15 @@ lval lval_err(int x) {
 void lval_print(lval v) {
     switch(v.type) {
         /* In the case the type is a number print it, then 'break' out of the switch. */
-        case LVAL_NUM: printf("%li", v.num); break;
+        case LVAL_NUM: printf("%li", v.data.num); break;
 
         /* In the case the type is an error */
         case LVAL_ERR:
             /* Check What exact type of error it is and print it */
-            printf("Error!");
-            if (v.err == LERR_DIV_ZERO) { printf("Error: Division by zero."); }
-            if (v.err == LERR_BAD_OP)   { printf("Error: Invalid operator."); }
-            if (v.err == LERR_BAD_NUM)  { printf("Error: Invalid number."); }
+            printf("Error: ");
+            if (v.data.err == LERR_DIV_ZERO) { printf("Division by zero."); }
+            if (v.data.err == LERR_BAD_OP)   { printf("Invalid operator."); }
+            if (v.data.err == LERR_BAD_NUM)  { printf("Invalid number."); }
         break;
     }
 }
@@ -80,20 +92,20 @@ lval eval_op(lval x, char* op, lval y) {
     if (y.type == LVAL_ERR) { return y; }
 
     /* Otherwise do maths on the number values */
-    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
-    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
-    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "+") == 0) { return lval_num(x.data.num + y.data.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.data.num - y.data.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.data.num * y.data.num); }
     if (strcmp(op, "/") == 0) {
         /* If second operand is zero return error instead of result */
-        return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num - y.num);
+        return y.data.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.data.num - y.data.num);
     }
-    if (strcmp(op, "%") == 0) { return lval_num(x.num % y.num); }
-    if (strcmp(op, "^") == 0) { return lval_num(pow(x.num, y.num)); }
+    if (strcmp(op, "%") == 0) { return lval_num(x.data.num % y.data.num); }
+    if (strcmp(op, "^") == 0) { return lval_num(pow(x.data.num, y.data.num)); }
     if (strcmp(op, "min") == 0) { 
-        return x.num < y.num ? lval_num(x.num) : lval_num(y.num);
+        return x.data.num < y.data.num ? lval_num(x.data.num) : lval_num(y.data.num);
     }
     if (strcmp(op, "max") == 0) {
-        return x.num > y.num ? lval_num(x.num) : lval_num(y.num);
+        return x.data.num > y.data.num ? lval_num(x.data.num) : lval_num(y.data.num);
     }
 
     return lval_err(LERR_BAD_OP);
@@ -112,7 +124,7 @@ lval eval(mpc_ast_t* t) {
     lval x = eval(t->children[2]);
 
     if (strcmp(op, "-") == 0 && !(strstr(t->children[3]->tag, "expr"))) {
-        return lval_num(-x.num);
+        return lval_num(-x.data.num);
     }
 
     int i = 3;
@@ -153,10 +165,11 @@ int main(int argc, char** argv) {
             lval result = eval(r.output);
             lval_println(result);
             mpc_ast_delete(r.output);
-        }/* else {    
+        } else {    
           mpc_err_print(r.error);
           mpc_err_delete(r.error);
-        } */
+        } 
+
         free(input);
         
     }
